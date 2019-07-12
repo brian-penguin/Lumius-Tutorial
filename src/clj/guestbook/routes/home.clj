2cc6e56bcbb634f0337f963b37140a69c779604c
Brian Tenggren <btenggren20@gmail.com>
2019-07-12 11:27:23 -0400

    Add some validation logic to our messages
    Why:
    * So we can return more meaningful messages about what's gone wrong
    
    This change is addresses this by:
    * Adding a struct core to use struct validations
    * Adding flash messages to the home route
    * adding flash messages to the html page form

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,20 +1,43 @@
 (ns guestbook.routes.home
   (:require
+   [struct.core :as st]
    [guestbook.layout :as layout]
    [guestbook.db.core :as db]
    [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
    [ring.util.http-response :as response]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:messages (db/get-messages)}))
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render
+   request
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
+; Here we are defining a struct that is the shape we expect our messages to be in.
+; We can use this with struct.core to validate that we are getting data in the correct shape
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string {:message "Must contain at least 10 characters"
+               :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
 (defn save-message! [{:keys [params]}]
-  (db/save-message! params)
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! params)
+      (response/found "/"))))
 
 (defn home-routes []
   [""
